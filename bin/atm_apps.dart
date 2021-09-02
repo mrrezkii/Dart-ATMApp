92df922dce6b5cf5708b7be8f200201d3ecb5342
nando <muhammad.rezki.ananda@gmail.com>
2021-09-02 23:15:19 +0700

    INITIAL COMMIT

---
+++ bin/atm_apps.dart
@@ -0,0 +1,5 @@
+import 'package:atm_apps/atm_apps.dart' as atm_apps;
+
+void main(List<String> arguments) {
+  print('Hello world: ${atm_apps.calculate()}!');
+}
---
+++ test/atm_apps_test.dart
@@ -0,0 +1,8 @@
+import 'package:atm_apps/atm_apps.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
