9bd1c554abe1368348013ea4037b4a1f363abe1a
nando <muhammad.rezki.ananda@gmail.com>
2021-09-03 11:12:44 +0700

    Finished

--- bin/atm_app.dart
+++ bin/atm_app.dart
@@ -19,30 +19,33 @@ void main(List<String> arguments) {
 
   var accountManager = AccountManager(LocalStorage());
 
-  print('''
+  while (true) {
+    print('''
   SELAMAT DATANG ATM BANK SOK JAGO
   ** LOGIN ***
      ''');
-  print('Masukkan username Anda : ');
-  var username = stdin.readLineSync();
-  var usernameFound =
-      (listUsername.contains(username)) ? listUsername.indexOf(username) : -1;
+    print('Masukkan username Anda : ');
+    var username = stdin.readLineSync();
+    var usernameFound =
+        (listUsername.contains(username)) ? listUsername.indexOf(username) : -1;
+    if (usernameFound == -1) {
+      print('\nAkun Anda tidak ditemukan\n\n');
+    } else {
+      print('Masukkan password Anda :');
+      var password = stdin.readLineSync();
 
-  print('Masukkan password Anda :');
-  var password = stdin.readLineSync();
+      var passwordValidation;
+      try {
+        passwordValidation =
+            (listPassword[usernameFound] == password) ? true : false;
+      } catch (e) {
+        return;
+      }
 
-  var passwordValidation;
-  try {
-    passwordValidation =
-        (listPassword[usernameFound] == password) ? true : false;
-  } catch (e) {
-    return;
-  }
+      if (passwordValidation) {
+        var accountNumber = listAccount[usernameFound];
 
-  if (passwordValidation) {
-    var accountNumber = listAccount[usernameFound];
-
-    print('''
+        print('''
 
     >> MENU YANG TERSEDIA
     1. Cek saldo
@@ -52,64 +55,67 @@ void main(List<String> arguments) {
     5. Log out
     ''');
 
-    while (true) {
-      print('Masukkan pilihan Anda  :');
-      var option = int.tryParse(stdin.readLineSync());
+        var continuously = true;
+        while (continuously) {
+          print('Masukkan pilihan Anda  :');
+          var option = int.tryParse(stdin.readLineSync());
 
-      switch (option) {
-        case 1:
-          print('Sisa Saldo Anda sebesar : Rp' +
-              accountManager.doCheckBalance(accountNumber).toString());
-          break;
-        case 2:
-          print('Masukkan nomor rekening yang di tuju');
-          var noAccount = stdin.readLineSync();
-          var indexAccount = (listNoAccount.contains(noAccount))
-              ? listNoAccount.indexOf(noAccount)
-              : -1;
-
-          if (indexAccount != -1) {
-            print('Masukkan nominal yang akan di transfer :');
-            var balance = int.tryParse(stdin.readLineSync());
-            var success = accountManager.doTransfer(
-                accountNumber, listAccount[indexAccount], balance);
-            if (success) {
-              print('Saldo berhasil di transfer');
-              print('Total saldo Anda sekarang Rp' +
+          switch (option) {
+            case 1:
+              print('Sisa Saldo Anda sebesar : Rp' +
                   accountManager.doCheckBalance(accountNumber).toString());
-            }
-          } else {
-            print('Nomor yang Anda masukkan salah');
+              break;
+            case 2:
+              print('Masukkan nomor rekening yang di tuju');
+              var noAccount = stdin.readLineSync();
+              var indexAccount = (listNoAccount.contains(noAccount))
+                  ? listNoAccount.indexOf(noAccount)
+                  : -1;
+
+              if (indexAccount != -1) {
+                print('Masukkan nominal yang akan di transfer :');
+                var balance = int.tryParse(stdin.readLineSync());
+                var success = accountManager.doTransfer(
+                    accountNumber, listAccount[indexAccount], balance);
+                if (success) {
+                  print('Saldo berhasil di transfer');
+                  print('Total saldo Anda sekarang Rp' +
+                      accountManager.doCheckBalance(accountNumber).toString());
+                }
+              } else {
+                print('Nomor yang Anda masukkan salah');
+              }
+              break;
+            case 3:
+              print('Masukkan nominal yang ingin Anda tarik :');
+              var balance = int.tryParse(stdin.readLineSync());
+              var success = accountManager.doWithdraw(accountNumber, balance);
+              if (success) {
+                print('Saldo berhasil ditarik');
+                print('Total saldo Anda sekarang Rp' +
+                    accountManager.doCheckBalance(accountNumber).toString());
+              }
+              break;
+            case 4:
+              print('Masukkan nominal yang ingin Anda setorkan :');
+              var balance = int.tryParse(stdin.readLineSync());
+              var success = accountManager.doAddBalance(accountNumber, balance);
+              if (success) {
+                print('Saldo berhasil ditambahkan');
+                print('Total saldo Anda sekarang Rp' +
+                    accountManager.doCheckBalance(accountNumber).toString());
+              }
+              break;
+            case 5:
+              continuously = false;
+              break;
+            default:
+              print('Anda memasukkan pilihan yang salah');
           }
-          break;
-        case 3:
-          print('Masukkan nominal yang ingin Anda tarik :');
-          var balance = int.tryParse(stdin.readLineSync());
-          var success = accountManager.doWithdraw(accountNumber, balance);
-          if (success) {
-            print('Saldo berhasil ditarik');
-            print('Total saldo Anda sekarang Rp' +
-                accountManager.doCheckBalance(accountNumber).toString());
-          }
-          break;
-        case 4:
-          print('Masukkan nominal yang ingin Anda setorkan :');
-          var balance = int.tryParse(stdin.readLineSync());
-          var success = accountManager.doAddBalance(accountNumber, balance);
-          if (success) {
-            print('Saldo berhasil ditambahkan');
-            print('Total saldo Anda sekarang Rp' +
-                accountManager.doCheckBalance(accountNumber).toString());
-          }
-          break;
-        case 5:
-          return;
-          break;
-        default:
-          print('Anda memasukkan pilihan yang salah');
+        }
+      } else {
+        print('\nPassword yang Anda masukkan salah\n\n');
       }
     }
-  } else {
-    print('\nAkun Anda tidak ditemukan');
   }
 }
